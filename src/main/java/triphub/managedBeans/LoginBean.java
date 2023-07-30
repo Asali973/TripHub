2d57a1cd9a633361c2135c8654bdcbd7b344eb07
BrenSim <brendan.simon@outlook.fr>
2023-07-30 17:04:45 +0200

    Commit avant merge

--- src/main/java/triphub/managedBeans/LoginBean.java
+++ src/main/java/triphub/managedBeans/LoginBean.java
@@ -82,5 +82,15 @@ public class LoginBean {
     public void setPassword(String password) {
         this.password = password;
     }
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+    
+    
 }
 
