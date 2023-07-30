f9cbe9478a782025d60d5869f5fc2a0ea3f276a3
MarieLvsq <maarielevesque@gmail.com>
2023-07-30 23:04:45 +0200

    Class&Enum_Transportation

---
+++ src/main/java/triphub/entity/product/service/transportation/Transportation.java
@@ -0,0 +1,63 @@
+package triphub.entity.product.service.transportation;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import triphub.entity.util.Address;
+
+@Entity
+public class Transportation {
+
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	private Address departure;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private Address arrival;
+	
+	@Enumerated(EnumType.STRING)
+	private TransportationType accommodation;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Address getDeparture() {
+		return departure;
+	}
+
+	public void setDeparture(Address departure) {
+		this.departure = departure;
+	}
+
+	public Address getArrival() {
+		return arrival;
+	}
+
+	public void setArrival(Address arrival) {
+		this.arrival = arrival;
+	}
+
+	public TransportationType getAccommodation() {
+		return accommodation;
+	}
+
+	public void setAccommodation(TransportationType accommodation) {
+		this.accommodation = accommodation;
+	}
+	
+	
+}
