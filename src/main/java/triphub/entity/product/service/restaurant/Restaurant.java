9de15b465c1458e19e89a9f194ecc7dc6792635c
BrenSim <brendan.simon@outlook.fr>
2023-07-31 10:28:37 +0200

    correction merge magdaline

--- src/main/java/triphub/entity/product/service/restaurant/Restaurant.java
+++ src/main/java/triphub/entity/product/service/restaurant/Restaurant.java
@@ -12,7 +12,7 @@ import triphub.entity.util.Address;
 public class Restaurant {
 	
 	@Id
-	@GeneratedValue (strategy = GenerationType.AUTO)
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@OneToOne
