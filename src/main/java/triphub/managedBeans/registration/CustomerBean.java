1a04790d698d1496bfae17a05f81ae355ce7ef1b
BrenSim <brendan.simon@outlook.fr>
2023-08-03 12:42:29 +0200

    Resolution pb COnfig

--- src/main/java/triphub/managedBeans/registration/CustomerBean.java
+++ src/main/java/triphub/managedBeans/registration/CustomerBean.java
@@ -10,7 +10,7 @@ import triphub.helpers.RegistrationException;
 import triphub.services.UserService;
 import triphub.viewModel.UserViewModel;
 
-@Named
+@Named("customerBean")
 @RequestScoped
 public class CustomerBean implements Serializable {
 
