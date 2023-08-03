24fd6e5e2247c74b70b6a3f53dca07b9cc0946c4
TwiTwiboo <nnphonemo@gmail.com>
2023-08-03 11:05:55 +0200

    to pull

--- src/main/java/triphub/main/main.java
+++ src/main/java/triphub/main/main.java
@@ -43,37 +43,48 @@
 //
 //	 EntityManager em = JPAUtil.getEntityManager();
 //
-////		em.getTransaction().begin();
-////		themeDao.create(theme);
-////		em.getTransaction().commit();
-////		
+//	 AddressDAO addressDao = new AddressDAO(em);
+//	 Address address1 = new Address();
+//		address1.setNum("12");
+//		address1.setStreet("rue de la gare");
+//		address1.setCity("Amiens");
+//		address1.setState("Haut de France");
+//		address1.setCountry("France");
+//		em.getTransaction().begin();
+//		addressDao.create(address1);
+//		em.getTransaction().commit();
+//		
+//		Address address2 = new Address();
+//		address2.setNum("24");
+//		address2.setStreet("rue de la gare");
+//		address2.setCity("Lille");
+//		address2.setState("Haut de France");
+//		address2.setCountry("France");
+//		em.getTransaction().begin();
+//		addressDao.create(address2);
+//		em.getTransaction().commit();
+//		
+//		Address address3 = new Address();
+//		address3.setNum("2");
+//		address3.setStreet("Piazza Napoleone");
+//		address3.setCity("Florence");
+//		address3.setState("Toscane");
+//		address3.setCountry("Italie");
+//		em.getTransaction().begin();
+//		addressDao.create(address3);
+//		em.getTransaction().commit();
+//		
 //
-////		TourPackage tourPackage = new TourPackage();
-////		tourPackage.setName("Summer Adventure");
-////		
-////		tourPackage.setDestination(destination);
-////		tourPackage.setTheme(theme);
-////		tourPackage.setPrice(price);
-////
-////		em.getTransaction().begin();
-////		tpDao.createOrUpdate(tourPackage);
-////		em.getTransaction().commit();
-////		
-////        System.out.println("Tourpackage:");
-////        System.out.println("Name: " + tourPackage.getName());
-////        System.out.println("Price: " + tourPackage.getPrice().getAmount() + " " + tourPackage.getPrice().getCurrency());
-////        System.out.println("Destination: " + tourPackage.getDestination().getCityName() + ", " + tourPackage.getDestination().getState() + ", " + tourPackage.getDestination().getCountry());
-////        System.out.println("Theme: " + tourPackage.getTheme().getName());
-//
-//
-////		Accommodation accommodation1 = new Accommodation();
-////		accommodation1.setNameAccommodation("Hotel Ibis");
-////		accommodation1.setAddress(address1);
-////		accommodation1.setAccommodation(AccommodationType.Hotel);
-////		em.getTransaction().begin();
-////		accommodationDao.create(accommodation1);
-////		em.getTransaction().commit();
-////		System.out.println("accommodation create "+ accommodation1.getNameAccommodation());
+//		AccommodationDAO accommodationDao= new AccommodationDAO(em);
+//		
+//		Accommodation accommodation1 = new Accommodation();
+//		accommodation1.setNameAccommodation("Hotel Ibis");
+//		accommodation1.setAddress(address1);
+//		accommodation1.setAccommodation(AccommodationType.Hotel);
+//		em.getTransaction().begin();
+//		//accommodationDao.create(accommodation1);
+//		em.getTransaction().commit();
+//		System.out.println("accommodation create "+ accommodation1.getNameAccommodation());
 //		
 //		
 //		//-------------------------------------- Restaurant ---------------------------------------------
@@ -102,59 +113,10 @@
 //		Restaurant restaurantTest2 = restaurantService.create(restaurantvm2);
 //		Restaurant restaurantTest3 = restaurantService.create(restaurantvm3);
 //		
-//		em.getTransaction().commit();
+//		em.getTransaction().commit();	
 //		
-//		
-//		//test tourpackage
-//		DestinationDAO desDao =new DestinationDAO(em);
-//		ThemeDAO themeDao= new ThemeDAO(em);
-//		PriceDAO priceDao= new PriceDAO(em);
-//		TourPackageDAO tpDao= new TourPackageDAO(em);
-//		
-//		Destination destination = new Destination();
-//		destination.setCityName("Paris");
-//		destination.setState("IDF");
-//		destination.setCountry("France");
-//		
-//		em.getTransaction().begin();
-//		desDao.create(destination);
-//		em.getTransaction().commit();
-//		
-//		Price price = new Price();
-//		price.setAmount(1500.0);
-//		price.setCurrency("USD");
-//		
-//		em.getTransaction().begin();
-//		priceDao.create(price);
-//		em.getTransaction().commit();
-//		
-//		Theme theme = new Theme("InstaSpots");
-//	
-//		
-//		em.getTransaction().begin();
-//		themeDao.create(theme);
-//		em.getTransaction().commit();
-//		
-//		TourPackage tourPackage = new TourPackage();
-//		tourPackage.setName("Summer Adventure");
-//		
-//		tourPackage.setDestination(destination);
-//		tourPackage.setTheme(theme);
-//		tourPackage.setPrice(price);
-//
-//		em.getTransaction().begin();
-//		tpDao.createOrUpdate(tourPackage);
-//		em.getTransaction().commit();
-//		
-//        System.out.println("Tourpackage:");
-//        System.out.println("Name: " + tourPackage.getName());
-//        System.out.println("Price: " + tourPackage.getPrice().getAmount() + " " + tourPackage.getPrice().getCurrency());
-//        System.out.println("Destination: " + tourPackage.getDestination().getCityName() + ", " + tourPackage.getDestination().getState() + ", " + tourPackage.getDestination().getCountry());
-//        System.out.println("Theme: " + tourPackage.getTheme().getName());
-//        System.out.println("Restau : " + restaurants);
-//
-//        
-//
+//	        
 //		JPAUtil.shutdown();
 //	}
 //
+//}
