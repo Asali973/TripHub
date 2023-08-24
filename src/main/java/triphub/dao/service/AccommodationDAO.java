d17a793ed065d70dbbe85d34d9fca2963b0ead92
Votre Nom Complet <j-francois_magdaline@hotmail.fr>
2023-08-24 18:57:53 +0200

    methodedeleteko

--- src/main/java/triphub/dao/service/AccommodationDAO.java
+++ src/main/java/triphub/dao/service/AccommodationDAO.java
@@ -40,77 +40,76 @@ public class AccommodationDAO {
 
 	public Accommodation create(SubServicesViewModel accommodationVm, Long userId, String userType) {
 
+		// Create Service
+		Service service = Service.createServiceFromViewModel(accommodationVm);
 
-	    // Create Service
-	    Service service = Service.createServiceFromViewModel(accommodationVm);
-    
-    		// Create Service
+		// Create Service
 		// Service service = Service.createServiceFromViewModel(accommodationVm);
 		// service.setType(ServiceType.ACCOMMODATION);
 
 		// Service service = new Service();
-    
-	    service.setType(ServiceType.ACCOMMODATION);
 
-	    // Create Price
-	    Price price = Price.createPriceFromViewModel(accommodationVm);
-	    service.setPrice(price);
+		service.setType(ServiceType.ACCOMMODATION);
 
-	    service.setAvailableFrom(accommodationVm.getAvailableFrom());
-	    service.setAvailableTill(accommodationVm.getAvailableTill());
-	    service.setAvailability(accommodationVm.isAvailability());
+		// Create Price
+		Price price = Price.createPriceFromViewModel(accommodationVm);
+		service.setPrice(price);
 
-	    // Persist Service and Price 
-	    em.persist(price);
-	    em.persist(service);
-	    
+		service.setAvailableFrom(accommodationVm.getAvailableFrom());
+		service.setAvailableTill(accommodationVm.getAvailableTill());
+		service.setAvailability(accommodationVm.isAvailability());
+
+		// Persist Service and Price
+		em.persist(price);
+		em.persist(service);
+
 		Picture picture = new Picture();
 		picture.setLink(accommodationVm.getLink());
 		accommodationVm.setPicture(picture);
 		em.persist(picture);
 
-	    // Create Accommodation
-	    Accommodation accommodation = new Accommodation();
-	    accommodation.setId(accommodationVm.getId());
-	    accommodation.setName(accommodationVm.getName());
-	    accommodation.setDescription(accommodationVm.getDescription());
-	    accommodation.setService(service);
-	    accommodation.setAccommodationType(accommodationVm.getAccommodationType());
+		// Create Accommodation
+		Accommodation accommodation = new Accommodation();
+		accommodation.setId(accommodationVm.getId());
+		accommodation.setName(accommodationVm.getName());
+		accommodation.setDescription(accommodationVm.getDescription());
+		accommodation.setService(service);
+		accommodation.setAccommodationType(accommodationVm.getAccommodationType());
 
-	    // Create Address
-	    Address addressAccommodation = new Address();
-	    addressAccommodation.setNum(accommodationVm.getAddress().getNum());
-	    addressAccommodation.setStreet(accommodationVm.getAddress().getStreet());
-	    addressAccommodation.setCity(accommodationVm.getAddress().getCity());
-	    addressAccommodation.setState(accommodationVm.getAddress().getState());
-	    addressAccommodation.setCountry(accommodationVm.getAddress().getCountry());
-	    addressAccommodation.setZipCode(accommodationVm.getAddress().getZipCode());
+		// Create Address
+		Address addressAccommodation = new Address();
+		addressAccommodation.setNum(accommodationVm.getAddress().getNum());
+		addressAccommodation.setStreet(accommodationVm.getAddress().getStreet());
+		addressAccommodation.setCity(accommodationVm.getAddress().getCity());
+		addressAccommodation.setState(accommodationVm.getAddress().getState());
+		addressAccommodation.setCountry(accommodationVm.getAddress().getCountry());
+		addressAccommodation.setZipCode(accommodationVm.getAddress().getZipCode());
 
-	    // Persist Address
-	    em.persist(addressAccommodation);
+		// Persist Address
+		em.persist(addressAccommodation);
 
-	    // Link the Address to the Accommodation
-	    accommodation.setAddress(addressAccommodation);
-	    
-	    if ("organizer".equals(userType)) {
-	        Organizer organizer = em.find(Organizer.class, userId);
-	        if (organizer == null) {
-	            throw new IllegalArgumentException("Organizer with ID " + userId + " not found.");
-	        }
-	        accommodation.setOrganizer(organizer); // Supposons que cette méthode existe
-	    } else if ("provider".equals(userType)) {
-	        Provider provider = em.find(Provider.class, userId);
-	        if (provider == null) {
-	            throw new IllegalArgumentException("Provider with ID " + userId + " not found.");
-	        }
-	        accommodation.setProvider(provider); // Supposons que cette méthode existe
-	    }
+		// Link the Address to the Accommodation
+		accommodation.setAddress(addressAccommodation);
 
-	    // Persist Accommodation
-	    em.persist(accommodation);
-	    em.flush();
+		if ("organizer".equals(userType)) {
+			Organizer organizer = em.find(Organizer.class, userId);
+			if (organizer == null) {
+				throw new IllegalArgumentException("Organizer with ID " + userId + " not found.");
+			}
+			accommodation.setOrganizer(organizer); // Supposons que cette méthode existe
+		} else if ("provider".equals(userType)) {
+			Provider provider = em.find(Provider.class, userId);
+			if (provider == null) {
+				throw new IllegalArgumentException("Provider with ID " + userId + " not found.");
+			}
+			accommodation.setProvider(provider); // Supposons que cette méthode existe
+		}
 
-	    return accommodation;
+		// Persist Accommodation
+		em.persist(accommodation);
+		em.flush();
+
+		return accommodation;
 
 	}
 
@@ -122,13 +121,12 @@ public class AccommodationDAO {
 		}
 
 		accommodation.updateAccommodationViewModel(accommodationvm);
-	
+
 		accommodation = em.merge(accommodation);
 		em.flush();
 
 		// Convert the updated entity back to the view model and return it
 		return accommodation.initAccommodationViewModel();
-	
 
 	}
 
@@ -174,17 +172,19 @@ public class AccommodationDAO {
 	public Accommodation findById(Long id) {
 		return em.find(Accommodation.class, id);
 	}
-	
+
 	public List<Accommodation> getAccommodationForOrganizer(Long organizerId) {
-	    TypedQuery<Accommodation> query = em.createQuery("SELECT tp FROM Accommodation tp WHERE tp.organizer.id = :organizerId", Accommodation.class);
-	    query.setParameter("organizerId", organizerId);
-	    return query.getResultList();
+		TypedQuery<Accommodation> query = em.createQuery(
+				"SELECT tp FROM Accommodation tp WHERE tp.organizer.id = :organizerId", Accommodation.class);
+		query.setParameter("organizerId", organizerId);
+		return query.getResultList();
 	}
-	
+
 	public List<Accommodation> getAccommodationForProvider(Long providerId) {
-	    TypedQuery<Accommodation> query = em.createQuery("SELECT tp FROM Accommodation tp WHERE tp.organizer.id = :providerId", Accommodation.class);
-	    query.setParameter("providerId", providerId);
-	    return query.getResultList();
+		TypedQuery<Accommodation> query = em.createQuery(
+				"SELECT tp FROM Accommodation tp WHERE tp.organizer.id = :providerId", Accommodation.class);
+		query.setParameter("providerId", providerId);
+		return query.getResultList();
 	}
 
 }
